57aafa3b166db3ff348f4dcb01db024b3ffa62ad
Jonnnuli <jonttuuh@hotmail.com>
2026-03-11 05:53:21 +0200

    Removed unnecessary comments

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-//const url = require("node:url");
-
-/*app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
-})*/
 
 blogsRouter.get('/', (request, response) => {
     Blog.find({}).then(blogs => {
