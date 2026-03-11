836b1f1f11f44ff90d0bef64a30d9ccbcafd663a
Jonnnuli <jonttuuh@hotmail.com>
2026-03-11 05:46:15 +0200

    Osa 4.1-4.2 toimii

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+//const express = require('express')
+//const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const app = express()
+//const app = express()
 
-const blogSchema = mongoose.Schema({
+/*const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,
     likes: Number,
-})
+})*/
 
-const Blog = mongoose.model('Blog', blogSchema)
+//const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://jonttuuh_db_user:8VMhBKz23FcAtsfB@cluster0.xuiqvvv.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0'
+//mongoose.connect(mongoUrl, { family: 4 })
 
-mongoose.connect(mongoUrl, { family: 4 })
+// app.use(express.json())
 
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
+/*app.get('/api/blogs', (request, response) => {
     Blog.find({}).then((blogs) => {
         response.json(blogs)
     })
@@ -32,10 +31,9 @@ app.post('/api/blogs', (request, response) => {
     blog.save().then((result) => {
         response.status(201).json(result)
     })
-})
+}) */
 
-const PORT = 3003
-app.listen(PORT, () => {
+//const PORT = 3003
+app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
-    console.log(`Server running on port ${PORT}`)
 })
